3fd54cefa0125016b9724aaefc03921c44f5be3f
Mochammad Alfain Kurniawan <alfainkn@gmail.com>
2021-12-07 23:28:12 +0700

    first commit

---
+++ server.R
@@ -0,0 +1,188 @@
+#
+# This is the server logic of a Shiny web application
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(tidytext)
+library(rtweet)
+
+prepare_data <- function(search, size) {
+    #get tweets
+    twitterdata <- search_tweets(
+        search, n = size, include_rts = FALSE
+    )
+    #if no data then rate exceeded
+    if (nrow(twitterdata) == 0) {
+        showNotification("Rtweet Rate limit exceeded. 
+                         Come back in 15 minutes:/",
+                         type = "warning"
+        )
+    }
+    
+    #simplify dataset
+    tweets.twitterdata = twitterdata %>% select(screen_name, text)
+    tweets.twitterdata
+    
+    #clean up dataset
+    head(tweets.twitterdata$text)
+    #replace html with empty space
+    tweets.twitterdata$stripped_text1 <- gsub("http\\S+","", tweets.twitterdata$text)
+    
+    #remove punctuation
+    tweets.twitterdata_stem <- tweets.twitterdata %>%
+        select(stripped_text1) %>%
+        #convert to a tidy dataset by representing each row with one token so we can use the tidytext library for sentiment analysis
+        #this function also converts text to lowercase and removes punctuation
+        unnest_tokens(word, stripped_text1)
+    
+    #remove stop words because they are not helpful for sentiment analysis
+    cleaned_tweets.twitterdata <- tweets.twitterdata_stem %>%
+        anti_join(stop_words)
+    
+    result <- list(cleaned_tweets.twitterdata, twitterdata)
+    return(result)
+}
+
+plot_top_10 <- function(search, n) {
+    list_data <- prepare_data(search, n)
+    Totallength <- n
+    data <- list_data[[1]]
+    
+    #begin constructing top 10 most frequent word plots
+    data %>%
+        count(word, sort=TRUE) %>%
+        #exclude #1 most common word because that will be the key word
+        slice(2:11) %>%
+        mutate(word = reorder(word, n)) %>%
+        ggplot(aes(x = word, y = n, fill=n)) +
+        scale_fill_gradient(low="blue", high="red") +
+        geom_col() +
+        xlab(NULL) +
+        coord_flip() +
+        theme_bw() +
+        labs(x = "Common Words",
+             y = "Frequency",
+             subtitle = sprintf('Not Including "%s" itself...', search),
+             title = sprintf('Top 10 Words in "%s" Tweets', search)) +
+        theme(plot.title = element_text(size = 20, face = "bold"),
+              plot.subtitle = element_text(size = 15, face = "italic"),
+              axis.title.x = element_text(size = 13),
+              axis.title.y = element_text(size = 13)) 
+}
+
+#set up sentiment function
+sentiment_bing = function(twt){
+    #clean tweet
+    twt_tbl = tibble(text = twt) %>%
+        mutate(
+            #remove html
+            stripped_text = gsub("http\\S+","",text)
+        ) %>%
+        unnest_tokens(word, stripped_text) %>%
+        anti_join(stop_words) %>%
+        inner_join(get_sentiments("bing")) %>%
+        count(word, sentiment, sort = TRUE) %>%
+        ungroup() %>%
+        #assign score based on sentiment
+        mutate(
+            score = case_when(
+                sentiment == 'negative'~n*(-1),
+                sentiment == 'positive'~n*1)
+        )
+    
+    validate_tibble(twt_tbl)
+    #calculate total score
+    sent.score = case_when(
+        nrow(twt_tbl)==0~0, #if no words then score is 0
+        nrow(twt_tbl)>0~sum(twt_tbl$score) #sum the pos and neg
+    )
+    #keep track of tweets with no words
+    zero.type = case_when(
+        nrow(twt_tbl)==0~"Type 1", #no words at all
+        nrow(twt_tbl)>0~"Type 2" #sum of words is 0
+    )
+    list(score = sent.score, type = zero.type, twt_tbl = twt_tbl)
+}
+
+
+sentiment_function <- function(search, n) {
+    list_data <- prepare_data(search, n)
+    data <- list_data[[1]]
+    original_data <- list_data[[2]]
+    
+    #apply function to dataset
+    data_sent = lapply(original_data$text, function(x){sentiment_bing(x)})
+    data_sent
+    
+    #use tidytext to get sentiment data
+    twitter_sentiment = bind_rows(
+        tibble(
+            name = search,
+            score = unlist(map(data_sent, 'score')),
+            type = unlist(map(data_sent, 'type'))
+        )
+    )
+    
+    #use ggplot to graph sentiments
+    ggplot(twitter_sentiment, aes(x=score, fill=..density..)) +
+        geom_histogram(binwidth=1) +
+        scale_fill_gradient(low="blue", high="purple") +
+        theme_bw() +
+        labs(x = "Sentiment Score",
+           y = "Frequency",
+           title = sprintf("Sentiment Score among %s Tweets", search),
+           subtitle= "The higher the sentiment, the more positive the tweet") +
+        theme(plot.title = element_text(size = 20, face = "bold"),
+            plot.subtitle = element_text(size = 15, face = "italic"),
+            axis.title.x = element_text(size = 13),
+            axis.title.y = element_text(size = 13)) 
+    
+}
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    query <- eventReactive(input$submit,
+                           {
+                             #query validation
+                             validate(
+                               need(input$searchquery != "", "Please fill out the search query")
+                             )
+                               input$searchquery
+                           })
+    
+    sentimentN <- eventReactive(input$submit,
+                                {
+                                    input$sentimentN
+                                })
+    
+    freqN <- eventReactive(input$submit,
+                                {
+                                    input$freqN
+                                })
+    
+    observeEvent(input$submit, {
+        output$plotFreq<-renderPlot({
+            plot_top_10(query()[[1]], freqN()[[1]])
+        })
+        
+        output$plotSent<-renderPlot({
+            sentiment_function(query()[[1]], sentimentN()[[1]])
+        })
+    })
+    
+    output$plotFreq<-renderPlot({
+        plot_top_10("#jokowi3periode", 1200)
+    })
+    
+    output$plotSent<-renderPlot({
+        sentiment_function("#jokowi3periode", 70)
+    })
+})
---
+++ ui.R
@@ -0,0 +1,61 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(rtweet)
+library(shinycustomloader)
+
+shinyUI(fluidPage(
+  tags$head(
+    # Note the wrapping of the string in HTML()
+    tags$style(HTML("
+      @import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
+      body {
+        background-color: #141d26;
+        color: white;
+      }
+      h2 {
+        font-family: 'Yusei Magic', sans-serif;
+      }
+      .shiny-input-container {
+        color: #474747;
+      }
+      .tab-content {
+        background-color: white;
+      }
+      .tabbable > .nav > li > a {background-color: #243447}
+      .tabbable > .nav > li > a:hover {background-color: white}
+      .tabbable > .nav > .active > a {background-color: white}
+      .shiny-notification {
+        position:fixed;
+        top: calc(25%);
+        left: calc(50%);
+        transform: translate(-50%, -50%);
+        padding: 20px;
+        font-size: 120%;
+      }"))
+  ),
+    titlePanel(title=h2("Twitter Data Analysis", align="center")),
+    
+    sidebarPanel(
+      textInput("searchquery", "Twitter Search Query: ", value="#jokowi3periode"),
+      sliderInput("sentimentN", "Tweet Sample Size for Sentiment Analysis: ", value=100, min=1, max=1600),
+      sliderInput("freqN", "Tweet Sample Size for Frequency Analysis: ", value=400, min=1, max=1600),
+      actionButton("submit", "Go!")
+    ),
+
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Sentiment Analysis", withLoader(plotOutput("plotSent"), type="image", loader="https://cdn.dribbble.com/users/908372/screenshots/4812323/loading2.gif")),
+        tabPanel("Frequency", withLoader(plotOutput("plotFreq"), type="image", loader="https://thumbs.gfycat.com/YellowishElementaryHind-max-1mb.gif")),
+        ##tabPanel("WordCloud", withLoader(plotOutput("wordcloud"), type="image", loader="https://thumbs.gfycat.com/YellowishElementaryHind-max-1mb.gif"))
+      )
+    ))
+)
